6cb2be0d1ab79c67f271ac221bd154386ffaeb62
Kamalanadha Reddy <143584583+kknr8367@users.noreply.github.com>
2025-09-03 12:23:37 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "ec2_sg" {
+  name        = "tf-sg"
+  description = "Allow inbound traffic on ports 80, 443, and 22"
+   
+  vpc_id = aws_vpc.main.id
+
+  
+  ingress {
+    description = "Allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+ 
+  ingress {
+    description = "Allow HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ 
+  ingress {
+    description = "Allow SSH traffic"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "tf-sg"
+  }
+}
